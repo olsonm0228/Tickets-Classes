f4a5b09cb4ac92149186cff582f4f4be74a4b172
Michael <olsonm0228@users.noreply.github.com>
2020-10-04 17:06:53 -0500

    create tickets

--- Ticket.cs
+++ Ticket.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tickets
+{
+    public class Tickets
+    {
+        /*
+            ticketId
+            TicketID, Summary, Status, Priority, Submitter, Assigned, Watching
+        */
+        public String ticketId{get;set;}
+        public String summary{get;set;}
+        public String status{get;set;}
+        public String priority{get;set;}
+        public String submitter{get;set;}
+        public String assigned{get;set;}
+        public List<String> watching{get;set;}
+
+        public Tickets(){
+            watching = new List<string>();
+        }
+
+        public String Display(){
+            string outputWatching = "";
+            for(int i = 0; i<watching.Count;i++){
+                if(i == watching.Count - 1){
+                    outputWatching += watching[i];
+                }else{
+                    outputWatching += watching[i] + ", ";
+                }
+            }
+            return $"Id: {ticketId}\nSummary: {summary}\nPriority: {priority}\nSubmitter: {submitter}\nAssigned: {assigned}\nWatching: {outputWatching}";
+        }
+    }
+}
